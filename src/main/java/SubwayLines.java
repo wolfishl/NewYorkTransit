5ba8115398d8dbb8d0b0b826f49485fa0578204f
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-10-21 18:32:40 -0400

    Added a json of the Subway Lines and a way to parse it

---
+++ src/main/java/SubwayLines.java
@@ -0,0 +1,52 @@
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class SubwayLines {
+    List<String> A;
+    List<String> B;
+    List<String> C;
+    List<String> D;
+    List<String> E;
+    List<String> F;
+    List<String> G;
+    List<String> H;
+    List<String> I;
+    List<String> J;
+    List<String> K;
+    List<String> L;
+    List<String> M;
+    List<String> N;
+    List<String> O;
+    List<String> P;
+    List<String> Q;
+    List<String> R;
+    List<String> S;
+    List<String> T;
+    List<String> U;
+    List<String> V;
+    List<String> W;
+    List<String> X;
+    List<String> Y;
+    List<String> Z;
+    @SerializedName("6 Express")
+    List<String> sixExpress;
+    @SerializedName("7 Express")
+    List<String> sevenExpress;
+    @SerializedName("1")
+    List<String> one;
+    @SerializedName("2")
+    List<String> two;
+    @SerializedName("3")
+    List<String> three;
+    @SerializedName("4")
+    List<String> four;
+    @SerializedName("5")
+    List<String> five;
+    @SerializedName("6")
+    List<String> six;
+    @SerializedName("7")
+    List<String> seven;
+
+
+}
---
+++ src/test/java/LinesTest.java
@@ -0,0 +1,32 @@
+import com.google.gson.Gson;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class LinesTest{
+
+    @Test
+    public void checkJson() throws IOException
+    {
+        //given
+        Gson gson = new Gson();
+        Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/SubwayLines.json"));
+
+        //when
+        SubwayLines lines = gson.fromJson(reader, SubwayLines.class);
+
+        //then
+        assertNotNull(lines);
+        assertEquals("55", lines.A.get(0));
+        assertEquals("247", lines.Z.get(0));
+        assertEquals("29", lines.sixExpress.get(0));
+        assertEquals("25", lines.seven.get(0));
+    }
+
+}
