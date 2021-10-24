6f60646990ea1e25d1372d94ceb67ea37f6f743f
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-10-24 10:41:01 -0400

    added code to add connecting stations, still must work on tests and changing to a hashmap

--- src/main/java/Stations.java
+++ src/main/java/Stations.java
@@ -1,3 +1,10 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Stations {
@@ -17,6 +24,45 @@ public class Stations {
         public String[] parseLines()
         {
             return line.split("-");
+        }
+
+        public List<String> getConnectingStations() throws IOException {
+            String[] thisStationLines = this.parseLines();
+            List<String> connectionStations =  new ArrayList<>();
+            Gson gson = new Gson();
+            Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/SubwayLines.json"));
+            SubwayLines lines = gson.fromJson(reader, SubwayLines.class);
+            lines.addToList();
+            for(String train : thisStationLines)
+            {
+                for (Train each : lines.allTrains)
+                {
+                    if (each.name == train)
+                    {
+                        addStations(connectionStations, each.stations);
+                        break;
+                    }
+                }
+            }
+            return connectionStations;
+        }
+
+        public void addStations(List<String> stations, List<String> train)
+        {
+            for (int i = 0; i < train.size(); i++)
+            {
+                if(train.get(i) == this.objectid)
+                {
+                    if (i != 0) {
+                        stations.add(train.get(i-1));
+                    }
+                    if(i != train.size()-1)
+                    {
+                        stations.add(train.get(i+1));
+                    }
+                    break;
+                }
+            }
         }
     }
 
--- src/main/java/SubwayLines.java
+++ src/main/java/SubwayLines.java
@@ -1,8 +1,10 @@
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SubwayLines {
+    List<Train> allTrains = new ArrayList<>();
     List<String> A;
     List<String> B;
     List<String> C;
@@ -47,6 +49,47 @@ public class SubwayLines {
     List<String> six;
     @SerializedName("7")
     List<String> seven;
+
+    public void addToList()
+    {
+        allTrains.add(new Train(A, "A"));
+        allTrains.add(new Train(B, "B"));
+        allTrains.add(new Train(C, "C"));
+        allTrains.add(new Train(D, "D"));
+        allTrains.add(new Train(E, "E"));
+        allTrains.add(new Train(F, "F"));
+        allTrains.add(new Train(G, "G"));
+        allTrains.add(new Train(H, "H"));
+        allTrains.add(new Train(I, "I"));
+        allTrains.add(new Train(J, "J"));
+        allTrains.add(new Train(K, "K"));
+        allTrains.add(new Train(L, "L"));
+        allTrains.add(new Train(M, "M"));
+        allTrains.add(new Train(N, "N"));
+        allTrains.add(new Train(O, "O"));
+        allTrains.add(new Train(P, "P"));
+        allTrains.add(new Train(Q, "Q"));
+        allTrains.add(new Train(R, "R"));
+        allTrains.add(new Train(S, "S"));
+        allTrains.add(new Train(T, "T"));
+        allTrains.add(new Train(U, "U"));
+        allTrains.add(new Train(V, "V"));
+        allTrains.add(new Train(W, "W"));
+        allTrains.add(new Train(X, "X"));
+        allTrains.add(new Train(Y, "Y"));
+        allTrains.add(new Train(Z, "Z"));
+        allTrains.add(new Train(sixExpress, "6 Express"));
+        allTrains.add(new Train(sevenExpress, "7 Express"));
+        allTrains.add(new Train(one, "1"));
+        allTrains.add(new Train(two, "2"));
+        allTrains.add(new Train(three, "3"));
+        allTrains.add(new Train(four, "4"));
+        allTrains.add(new Train(five, "5"));
+        allTrains.add(new Train(six, "6"));
+        allTrains.add(new Train(seven, "7"));
+    }
+
+
 
 
 }
